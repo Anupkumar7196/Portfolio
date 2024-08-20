6739f52f570c54718fb6b8e39b4983ae7b1c3883
Anup kumar <87388940+Anupkumar7196@users.noreply.github.com>
2024-08-21 01:07:04 +0530

    Add files via upload

---
+++ Portfolio/Portfolio.js
@@ -0,0 +1,23 @@
+// --------------------Typing Animation
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Software Developer","Web Developer"],
+    typeSpeed: 100,
+    BackSpeed:100,
+    loop:true
+})
+
+//---------SEnd email----------
+// function sendEmail(){
+//     Email.send({
+//     Host : "smtp.gmail.com",
+//     Username : "anupkumar2820199@gmail.com",
+//     Password : "Anupkumar123",
+//     To : 'anupk7196@gmail.com',
+//     From : document.getElementById(email).value,
+//     FullName: document.getElementById(FullName).value,
+//     Subject : "New Contact From Enquiry",
+//     Body : "And this is the body"
+// }).then(
+//   message => alert(message)
+// );
+// }
